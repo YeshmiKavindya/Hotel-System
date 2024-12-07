bb227d507b56250858635f48e05adb48c06f3e90
Yasas Ferreira <139609496+YasasFerreira@users.noreply.github.com>
2024-12-07 07:42:19 +0000

    created button for index

--- php/functions/navbar.php
+++ php/functions/navbar.php
@@ -1,6 +1,12 @@
 <?php
     function navigationbar(){
-        
+        echo "<div class='collapse navbar-collapse' id='navbarNav'>";
+        echo "<ul class='navbar-nav ms-auto'>";
+              echo "<li class='nav-item'>";
+                echo "<a class='nav-link ' href='../php/index.php' style='color: darkgoldenrod;font-family: 'Times New Roman', Times,serif'>HOME</a>";
+              echo "</li>";
+        echo "</ul>";
+        echo "</div>";
 
     }
 ?>
