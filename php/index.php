1d264321e5f617fcda9d9981f1f646c50b72af3a
Yasas Ferreira <139609496+YasasFerreira@users.noreply.github.com>
2024-12-07 08:16:36 +0000

    connect navbar.php to index .php

--- php/index.php
+++ php/index.php
@@ -40,7 +40,9 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <>
+    <?php
+      require_once 'functions/navbar.php';
+    ?>
   </div>
 </nav>
 
