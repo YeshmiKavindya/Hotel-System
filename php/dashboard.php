37e07ef87f43b9f40eaa2f9d9ae4e4bbc51d9fe1
Yasas Ferreira <yasasferreira05@gmail.com>
2024-12-10 17:25:38 +0530

    link dashborad.css

--- php/dashboard.php
+++ php/dashboard.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <link href="../bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../css/dashboard.css">
 </head>
 <body>
     <div class="row">
         <div class="col3"></div>
-        <div class="col"></div>
+        <div class="col">
+            <center><h4>DASHBOARD</h4></center>
+        </div>
     </div>
     <script src="bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
 </body>
