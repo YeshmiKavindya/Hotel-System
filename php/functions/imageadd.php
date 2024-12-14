85c99083796d106d8d8ba5cee88640a1c1063f31
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2024-12-14 14:00:49 +0000

    updated addImage()

--- php/functions/imageadd.php
+++ php/functions/imageadd.php
@@ -1,30 +1,50 @@
 <?php
 
-    function addImage($image1,$image2,$image3){
+function addImage($id1, $id2, $id3) {
+    
+  require_once 'conf.php';
 
-        echo '<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="'.$image1.'" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src= "'.$image2.'" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src= "'.$image3.'" class="d-block w-100" alt="...">
-                  </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
-        
-        
-        ';
-    }
+  
+  $sql = "SELECT file_path FROM images WHERE imageID IN ('$id1', '$id2', '$id3')";
+  $result = mysqli_query($connect, $sql);
+
+  
+  if (mysqli_num_rows($result) > 0) {
+      
+      $images = [];
+      while ($row = mysqli_fetch_assoc($result)) {
+          $images[] = $row['file_path'];
+      }
+
+     
+      if (count($images) == 3) {
+          
+          echo '<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner">';
+
+          
+          foreach ($images as $index => $image) {
+              $activeClass = ($index === 0) ? 'active' : ''; 
+              echo '<div class="carousel-item ' . $activeClass . '">
+                      <img src="' . $image . '" class="d-block w-100" alt="...">
+                    </div>';
+          }
+
+         
+          echo '  </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
+                </div>';
+      } 
+  }
+
+
+  mysqli_close($connect);
+}
 ?>
