dd02f04805ac746e8b3e9213d2df7b04cc16c279
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2024-12-22 15:06:57 +0000

    updated

--- php/restaurant.php
+++ php/restaurant.php
@@ -87,47 +87,14 @@
             style="font-family: 'Times New Roman'; background-color: #f3f3e0"
           >
             <h5 class="card-title">Sri Lankan Cuisine</h5>
-            <div
-              id="carouselExampleAutoplaying"
-              class="carousel slide"
-              data-bs-ride="carousel"
-            >
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="../images/77.jpg" class="d-block w-100" alt="..." />
-                </div>
-                <div class="carousel-item">
-                  <img src="../images/88.jpg" class="d-block w-100" alt="..." />
-                </div>
-                <div class="carousel-item">
-                  <img src="../images/78.jpg" class="d-block w-100" alt="..." />
-                </div>
-              </div>
-              <button
-                class="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="prev"
-              >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button
-                class="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="next"
-              >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
+            <!-- add image -->
+            <div><?php
+                require_once 'functions/dbconf.php';
+                require_once 'functions/imageadd.php';
+
+                addImage(30,41,31,$connect);
+
+            ?></div>
             <div class="row">
               <div class="col-sm-6 col-md-5 col-lg-6">
                 <ul>
@@ -193,47 +160,16 @@
             style="font-family: 'Times New Roman'; background-color: #f3f3e0"
           >
             <h5 class="card-title">Chinese Cuisine</h5>
-            <div
-              id="carouselExampleAutoplaying"
-              class="carousel slide"
-              data-bs-ride="carousel"
-            >
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="../images/76.jpg" class="d-block w-100" alt="..." />
-                </div>
-                <div class="carousel-item">
-                  <img src="../images/75.jpg" class="d-block w-100" alt="..." />
-                </div>
-                <div class="carousel-item">
-                  <img src="../images/79.jpg" class="d-block w-100" alt="..." />
-                </div>
-              </div>
-              <button
-                class="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="prev"
-              >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button
-                class="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="next"
-              >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
+            
+            <!-- add image -->
+            <div><?php
+                require_once 'functions/dbconf.php';
+                require_once 'functions/imageadd.php';
+
+                addImage(29,28,32,$connect);
+
+            ?></div>
+      
             <div class="row">
               <div class="col-sm-6 col-md-5 col-lg-6">
                 <ul>
