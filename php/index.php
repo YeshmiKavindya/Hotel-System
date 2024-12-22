f332f5ba140579959c47054afaf3797c1ca0d9d6
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2024-12-22 14:51:54 +0000

    updated

--- php/index.php
+++ php/index.php
@@ -56,30 +56,6 @@
   addImage(10,8,9,$connect);
 ?>
 
-<!-- <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../images/49.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="../images/44.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="../images/41.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-
-</div> -->
-
-
 <div class="card text-center" style=" background-color: #FFDBB5;font-family: 'Times New Roman', Times,serif;border-radius: 0%;" >
   <div class="card-body">
     <h3 class="card-title" >WELCOME TO YARROW HEIGHTS HOTEL</h3>
