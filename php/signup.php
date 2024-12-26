73fc2e081790f606b9a68563d4f7a67cc6cc5c8b
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2024-12-26 18:07:07 +0000

    created and updated signup.php

---
+++ php/signup.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="../css/signin.css">
+    <title>Signin</title>
+</head>
+<body>
+
+
+    
+        <div class="login">
+            <div class="content text-and text-bg-secondary p-3 bg-opacity-50 bg-body-tertiary">
+                <form action="" method="post" style="color: black;margin:2em;padding:2em;">
+                    <center><h3>Login</h3></center>
+                    <br>
+                    <div >
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" name="username"required/>
+                    </div>
+                    <br>
+                    <div >
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" name="email"required/>
+                    </div>
+                    <br>
+                    <div>
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control"  name="password" id="pword" required/>
+                    </div>
+                    <div>
+                        <label for="confirmpassword">Confirm Password</label>
+                        <input type="password" class="form-control"  name="confirmpassword" id="pword" required/>
+                    </div>
+                    <div>
+                        <input type="checkbox" class="form-check-input" name=""  onclick="show()">
+                        <label for="showpassword" style="font-weight:bold;">show password</label>
+                    </div>
+                    
+                    <br>
+
+                    <div class="d-grid gap-2 col-4 mx-auto">
+                        <input type="submit" name="submit" value="Sign in" class="form-control">
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    
+    
+
+        <script type="text/javascript">
+            function show(){
+                var input = document.getElementById('pword')
+                if(input.type == "password"){
+                    input.type='text'
+                }else{
+                    input.type='password'
+                }
+
+            }
+
+        </script>
+    
+        <script src="../bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
