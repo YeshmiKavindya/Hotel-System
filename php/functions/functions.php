4042b746a13569f8cc8ad49bb89579671f3af773
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2025-01-01 10:24:36 +0000

    function for restaurant

--- php/functions/functions.php
+++ php/functions/functions.php
@@ -1,13 +1,13 @@
 <?php
 
 require_once 'dbconf.php';
-    function showDetails($connect){
+    function showDetailsRooms($connect){
 
         
 
         try{
             
-            $sql = "SELECT * from Rooms";
+            $sql = "SELECT * from rooms";
         $result = mysqli_query($connect,$sql);
 
         $numres = mysqli_num_rows($result);
@@ -46,4 +46,53 @@ require_once 'dbconf.php';
 
         
 }
+
+
+
+function showDetailsRestaurant($connect){
+
+        
+
+    try{
+        
+        $sql = "SELECT * from restaurant";
+    $result = mysqli_query($connect,$sql);
+
+    $numres = mysqli_num_rows($result);
+
+    echo "
+        <table>
+        <thead>
+            <th>RoomID</th>
+            <th>RoomType</th>
+            <th>PricePerNight</th>
+            <th>Capacity</th>
+            <th>Status</th>
+        </thead>
+        <tbody>
+    ";
+     
+
+    for($i=$numres-5;$i<$numres;$i++){
+        $row = mysqli_fetch_assoc($result);
+        echo "<tr>";
+        foreach($row as $key){
+            
+                echo "<td>".$key."</td>";
+            }
+        
+        echo "</tr>";
+    }
+
+    echo "</tbody>
+        </table>";
+
+        mysqli_close($connect);
+}catch(Exception $ex){
+
+}
+
+    
+}
+
 ?>
