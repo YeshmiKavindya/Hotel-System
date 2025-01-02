76a475d766f8e5be4184218a3b04c6f7caefd835
Yasas Ferreira <139609496+YasasFerreira@users.noreply.github.com>
2025-01-02 08:54:25 +0000

    create func for card

--- php/functions/functions.php
+++ php/functions/functions.php
@@ -80,16 +80,34 @@ function showDetailsRestaurant($connect){
 }   
 }
 
-function foodDetails(){
+function foodDetails($connect){
 
-    echo "<div class='card' style='width: 15rem;'>
-    <img src='..' class='card-img-top' alt='...'>
-    <div class='card-body'>
-        <h5 class='card-title'>Card title</h5>
-        <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href='#' class='btn btn-primary'>Go somewhere</a>
-    </div>
-    </div>";
+    $sql = "SELECT file_path FROM images where description like '%food%'";
+    $result = mysqli_query($connect,$sql);
+
+    $numrecords = mysqli_num_rows($result);
+
+    echo "$numrecords";
+
+    for ($i=0;$i<$numrecords;$i++){
+
+        $data = mysqli_fetch_assoc($result);
+        foreach($data as $filepath){
+            echo "<div class='card' style='width: 15rem;'>
+            <img src='$filepath' class='card-img-top' alt='...'>
+            <div class='card-body'>
+                <h5 class='card-title'>Card title</h5>
+                <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href='#' class='btn btn-primary'>Go somewhere</a>
+            </div>
+            </div>";
+
+
+        }
+
+    }
+
+    
 }
 
 ?>
