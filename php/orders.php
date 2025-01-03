cc3598d93ce23937ca2e9ce8f71916bd8c17807f
Yasas Ferreira <yasasferreira05@gmail.com>
2025-01-03 18:55:52 +0530

    add navbar

--- php/orders.php
+++ php/orders.php
@@ -4,10 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>orders</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/style.css'>
+    <link href="../bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
-    
-    
+    <!-- <add navbar> -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: black;">
+        <div class="container-fluid">
+         
+          <?php
+            require_once 'functions/navbar.php';
+            addLogo();
+            addToggleButton();
+            navigationbar();
+          ?>
+        </div>
+      </nav> 
+
+      
+
+    <script src="../bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
