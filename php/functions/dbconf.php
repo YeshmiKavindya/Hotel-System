7e49616fb18589e02d6bf3180287bf46330d90a9
Yasas Ferreira <yasasferreira05@gmail.com>
2025-01-03 17:58:19 +0530

    updated

--- php/functions/dbconf.php
+++ php/functions/dbconf.php
@@ -1,8 +1,13 @@
 <?php
-    define('SERVERNAME','127.0.0.0:3306');
-    define('USERNAME','root');
-    define('PASSWORD','mariadb');
+    // define('SERVERNAME','127.0.0.0:3306');
+    // define('USERNAME','root');
+    // define('PASSWORD','mariadb');
     define('DBNAME','hotelsystem');
+    define('SERVERNAME','localhost');
+    define('USERNAME','root');
+    define('PASSWORD','');
+
+
 
     $connect=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
 
